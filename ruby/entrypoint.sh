ca22e8eef1687bf5d4f2621eaffc4225c7dc5233
Roberto Giovanardi <r.giovanardi@advenias.it>
2021-07-19 10:03:37 +0200

    zsh customizations

--- ruby/entrypoint.sh
+++ ruby/entrypoint.sh
@@ -1,1 +1,10 @@
-echo "alias ll=\'ls -l\'" >> ~/.bashrc
+# setup alias to bashrc
+echo "alias ll='ls $LS_OPTIONS -l'" >> ~/.bashrc
+echo "alias l='ls $LS_OPTIONS -lA'" >> ~/.bashrc
+
+
+# setup ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# change theme to arrow
+sed -i -E "s/(ZSH_THEME=).*/\1\"arrow\"/" ~/.zshrc
